7ce3f5d6eac60a05600c157189c4448e765dfdec
ciawalsh <cia.walsh@gmail.com>
2015-02-06 15:57:22 +0000

    1-4 passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -5,14 +5,17 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.select { |word| word.start_with?('a','e','i','o','u') }
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.compact
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array2 = array.compact ; array2.reject { |word| word == false }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
